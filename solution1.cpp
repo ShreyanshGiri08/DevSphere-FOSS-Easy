abf816744703a62736fdc875fb888920ab9f9319
Shreyansh Giri <girilakshmi91@gmail.com>
2026-04-14 18:05:23 +0530

    Revert "I feel this is the correct commit"
    This reverts commit db391e945c7ac92f89ea9f15f24da104d9fc1eaa.

--- solution1.cpp
+++ solution1.cpp
@@ -6,12 +6,7 @@ int main() {
     while(t--){
         long long n, k;
         cin >> n >> k;
-        long long count = 0, current = 0;
-        while (count < k) {
-            current++;
-            if (current % n != 0) count++;
-        }
-        cout << current << endl;
+        cout << k + (k / n) << endl; 
     }
     return 0;
 }
